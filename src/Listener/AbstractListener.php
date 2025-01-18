90128bf48bd10ab7aa6fc2a1ba6b5cb1b0cf2d58
Willem Verspyck <willemverspyck@users.noreply.github.com>
2025-01-18 16:56:37 +0100

    Check amounts and round

--- src/Listener/AbstractListener.php
+++ src/Listener/AbstractListener.php
@@ -4,34 +4,36 @@ declare(strict_types=1);
 
 namespace Spyck\AccountingBundle\Listener;
 
+use Exception;
 use Spyck\AccountingBundle\Entity\Invoice;
 
 abstract class AbstractListener
 {
     protected function patchInvoice(Invoice $invoice): void
     {
-        if (null !== $invoice->getCode()) {
-            return;
-        }
-
-        $amount = [];
-        $amountTax = [];
+        $amount = 0.0;
+        $amountTax = 0.0;
 
         foreach ($invoice->getJobs() as $job) {
             $service = $job->getService();
 
-            $job->setAmount($service->getAmount());
             $job->setTax($service->hasTax());
             $job->setTaxRate($service->getTaxRate());
 
-            $amount[] = $job->getQuantity() * $job->getAmount();
-            $amountTax[] = $job->hasTax() ? $job->getQuantity() * $job->getAmount() * $job->getTaxRate() : 0;
+            $amount += round($job->getQuantity() * $job->getAmount(), 2);
+            $amountTax += $job->hasTax() ? round($job->getQuantity() * $job->getAmount() * $job->getTaxRate(), 2) : 0;
+        }
+
+        if (null !== $invoice->getCode()) {
+            if ($amount !== $invoice->getAmount() || $amountTax !== $invoice->getAmountTax()) {
+                throw new Exception('Invoice closed');
+            }
         }
 
         $customer = $invoice->getCustomer();
 
-        $invoice->setAmount(array_sum($amount));
-        $invoice->setAmountTax(array_sum($amountTax));
+        $invoice->setAmount($amount);
+        $invoice->setAmountTax($amountTax);
         $invoice->setData([
             'name' => $customer->getName(),
             'contact' => $customer->getContact(),
